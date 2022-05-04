26d73a03201c17756f0d3def5e75d70e2fd64c29
Gabriela Suchopárová <gabi.suchoparova@gmail.com>
2022-05-04 12:10:33 +0200

    Draft with continuous actions.

--- marketsim/msstrategies/neuralnetstrategy.hpp
+++ marketsim/msstrategies/neuralnetstrategy.hpp
@@ -1,34 +1,64 @@
 #include "marketsim.hpp"
+#include <boost/math/constants/constants.hpp>
 #include "nets/actorcritic.hpp"
 
 namespace marketsim {
+
+    torch::Tensor action_log_prob(torch::Tensor mu, torch::Tensor std, torch::Tensor x) {
+        auto variance = std.pow(2);
+        auto log_scale = std.log();
+        auto subs = (-(x - mu).pow(2) / (2 * variance));
+        return subs - log_scale - std::log(std::sqrt(2 * boost::math::constants::pi<double>()));
+    }
+
     class neuralnetstrategy : public teventdrivenstrategy {
     public:
         neuralnetstrategy() : teventdrivenstrategy(1),
             net(std::make_unique<ActorCriticNet>()),
-            optimizer(std::make_unique<torch::optim::SGD>(net->parameters(), /*lr=*/0.01)) {}
+            optimizer(std::make_unique<torch::optim::SGD>(net->parameters(), /*lr=*/0.01)),
+            history() {}
 
     private:
         virtual trequest event(const tmarketinfo& mi, tabstime t, trequestresult* lastresult) {
-            torch::Tensor some_state = torch::rand({32, 20});
+            if (history.size() > 0) {
+                
+            }
 
-            auto pred = net->forward(some_state);
-            auto action_log_probs = std::get<0>(pred);
-            auto state_value = std::get<1>(pred);
+            // TODO ulož si prev state, action, spočítej reward (to je něco jako diff)
+            // one step boostrap to je jako network pred value next state
 
-            auto advantages = (torch::rand({32, 1}) - state_value);
-            auto value_loss = advantages.pow(2).mean();
-            auto action_loss = -(advantages.detach() * action_log_probs).mean();
             
-            auto loss = value_loss + action_loss; //TODO entropy
+            // TODO
+            //   - state dostanu jako current state (proste veci z marketu atd)
+            //   - action si napredikuju
+            //   - a v tom ifu nahore si vemu posledni stav a akci, napredikuju value next_state a uz mam co potrebuju
+            //      - akorat reward je asi diff, takze neco z minulyho si asi taky ulozim
 
-            optimizer->zero_grad();
-            loss.backward();
-            optimizer->step();
+            //history.push_back(std::make_pair(state, action));
 
             return trequest();
         }
 
+        void train(torch::Tensor state, torch::Tensor actions, torch::Tensor returns) {
+            
+
+            auto pred = net->forward(state);
+            auto action_mus = std::get<0>(pred);
+            auto action_stds = std::get<1>(pred);
+            auto state_value = std::get<2>(pred);
+
+            auto advantages = (returns - state_value);
+            auto value_loss = advantages.pow(2).mean(); //TODO minus dole správně?
+            auto action_loss = -(advantages.detach() * action_log_prob(action_mus, action_stds, actions).sum()).mean();
+            
+            auto loss = value_loss + action_loss; //TODO entropy regularization
+
+            optimizer->zero_grad();
+            loss.backward();
+            optimizer->step();
+        }
+
+        std::vector<std::pair<torch::Tensor, torch::Tensor>> history;
         std::unique_ptr<ActorCriticNet> net;
         std::unique_ptr<torch::optim::SGD> optimizer;
     };
--- marketsim/nets/actorcritic.hpp
+++ marketsim/nets/actorcritic.hpp
@@ -5,26 +5,51 @@ namespace marketsim {
     struct ActorCriticNet : torch::nn::Module {
         ActorCriticNet() :
             state_size(20),
-            action_size(20),
+            action_size(3),
             hidden_size(128)
         {
             //TODO state_size?
             linear = register_module("linear", torch::nn::Linear(state_size, hidden_size));
             critic = register_module("critic", torch::nn::Linear(hidden_size, 1));
-            actor = register_module("actor", torch::nn::Linear(hidden_size, action_size));
+            actor_mu = register_module("actor_mu", torch::nn::Linear(hidden_size, action_size));
+            actor_std = register_module("actor_std", torch::nn::Linear(hidden_size, action_size));
+
         }
 
-        std::pair<torch::Tensor, torch::Tensor> forward(torch::Tensor x) {
-            x = torch::relu(linear->forward(x));
+        std::pair<torch::Tensor, torch::Tensor> predict_actions(torch::Tensor x, bool linear) {
+            if (linear) {
+                x = pred_linear(x);
+            }
+            auto mu = actor_mu->forward(x);
+            auto std = actor_std->forward(x);
+            return std::make_pair<>(mu, std);
+        }
+
+        torch::Tensor predict_values(torch::Tensor x, bool linear) {
+            if (linear) {
+                x = pred_linear(x);
+            }
+            return critic->forward(x);
+        }
+
+        torch::Tensor pred_linear(torch::Tensor x) {
+            return torch::relu(linear->forward(x));
+        }
+
+        std::tuple<torch::Tensor, torch::Tensor, torch::Tensor> forward(torch::Tensor x) {
+            x = pred_linear(x);
             
-            auto action_prob = torch::log_softmax(actor->forward(x), /*dim=*/-1);  //TODO dim?
-            auto state_values = critic->forward(x);
-            return std::make_pair(action_prob, state_values);
+            auto actions = predict_actions(x, false);
+            auto action_mu = std::get<0>(actions);
+            auto action_std = std::get<1>(actions);
+            auto state_values = predict_values(x, false);
+            return std::make_tuple<>(action_mu, action_std, state_values);
         }
 
         int state_size;
         int hidden_size;
         int action_size;  //TODO zjistit jak consumption atd (viz ten clanek)
-        torch::nn::Linear linear{nullptr}, critic{nullptr}, actor{nullptr};
+        torch::nn::Linear linear{nullptr}, critic{nullptr};
+        torch::nn::Linear actor_mu{nullptr}, actor_std{nullptr};
     };
 }
